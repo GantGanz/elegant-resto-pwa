061687acbba11b4b822e99e14183963d9bbfbcf7
GantGanz <anggerelegant7@gmail.com>
2021-05-25 22:51:14 +0700

    add hero image

--- webpack.common.js
+++ webpack.common.js
@@ -23,6 +23,12 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
     ],
   },
   plugins: [
