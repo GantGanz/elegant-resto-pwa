cee885e927c922af41c5d33b56e1e19a99778a9b
GantGanz <anggerelegant7@gmail.com>
2021-05-26 19:57:33 +0700

    hero hanya di list

--- src/scripts/views/pages/list.js
+++ src/scripts/views/pages/list.js
@@ -5,6 +5,12 @@ import { createRestaurantItemTemplate } from '../templates/template-creator';
 const List = {
   async render() {
     return `
+      <div class="hero">
+        <div class="hero__inner">
+          <h1 class="hero__title">Elegant Restaurant Reviews</h1>
+          <p class="hero__tagline">Review singkat dari berbagai macam restaurant yang ada di seluruh Indonesia</p>
+        </div>
+      </div>
       <div class="content">
         <h2 class="content__heading">List of Restaurants</h2>
         <div id="restaurants" class="restaurants">
